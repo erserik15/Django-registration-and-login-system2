43f8685e7c5aa2870a9ddda8349d7c2657f5c347
erserik15 <76200359+erserik15@users.noreply.github.com>
2023-06-26 04:13:57 +0600

    Update urls.py
    index

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, profile, RegisterView
+from .views import home, profile, RegisterView, index
 
 urlpatterns = [
     path('', home, name='users-home'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
+    path('/static/users/index.html', index),
 ]
